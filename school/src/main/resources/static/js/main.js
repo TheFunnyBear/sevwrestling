b5b0f6a4d3108d544b04033562b1670abbccccc5
Denis <denisisvodolaz@yandex.ru>
2019-12-23 18:40:27 +0300

    Add some vue.js UI
    Добавление простой формы на UI

--- school/src/main/resources/static/js/main.js
+++ school/src/main/resources/static/js/main.js
@@ -1,6 +1,39 @@
+Vue.component('messagesItem', {
+    props: ['message'],
+    template:
+        '<li>' +
+        '   <div>' +
+        '       <b>{{ message.labelMessage }}</b>' +
+        '      <br/>' +
+        '      <i>{{message.textMessage}}</i>' +
+        '   </div>' +
+        '</li>'
+})
+
+
+Vue.component('messagesList', {
+    props: ['messages'],
+    template:
+        '<div>' +
+        '   <h3>Это список сообщений:</h3>\n' +
+        '   <div id="app-4">\n' +
+        '      <ol>\n' +
+        '         <messagesItem v-for="message in messages" v-bind:key="message.id" :message="message"/>\n' +
+        '      </ol>\n' +
+        '   </div>' +
+        '</div>'
+})
+
+
+
 var app = new Vue({
     el: '#app',
+    template: '<messagesList :messages="messages"/>',
     data: {
-        message: 'Привет, Vue!'
+        messages: [
+            {id: '1', labelMessage: 'message 1', textMessage: 'Привет, Vue! 1'},
+            {id: '2', labelMessage: 'message 2', textMessage: 'Привет, Vue! 2'},
+            {id: '3', labelMessage: 'message 3', textMessage: 'Привет, Vue! 3'},
+        ]
     }
 });
