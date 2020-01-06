973b6ea53e7365736b611653d88fc21b8489f895
Denis <denis@kosolapov.info>
2020-01-06 09:19:45 +0300

    Move routing to UI
    Перенос роутинга на UI

---
+++ school-ui/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import routes from './routes'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.pathname
+  },
+  computed: {
+    ViewComponent () {
+      const matchingView = routes[this.currentRoute]
+      return matchingView
+        ? require('./pages/' + matchingView + '.vue')
+        : require('./pages/404.vue')
+    }
+  },
+  render (h) {
+    return h(this.ViewComponent)
+  }
+})
+
+window.addEventListener('popstate', () => {
+  app.currentRoute = window.location.pathname
+})
